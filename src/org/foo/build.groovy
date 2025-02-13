2a6ec30825257c58952a6fa1be5d1e13831384c0
呦呦狸花花 <youyoulihuahua@youyoulihuahuadeMac-mini.local>
2025-02-13 22:26:30 +0800

    add file

---
+++ src/org/foo/build.groovy
@@ -0,0 +1,8 @@
+package org.foo
+
+def Build(buildType,buildShell){
+    def buildTools=["maven":"M2","ant":"ANT","gradle":"GRADLE"]
+    println("当前选择的构建类型是${buildType}")
+    buildHome=tool buildTools[buildType]
+    sh "${buildHome}/bin/${buildType} ${buildShell}"
+}
