274c906db7236b51cf414ef34b04b4b614a2f749
呦呦狸花花 <youyoulihuahua@youyoulihuahuadeMac-mini.local>
2025-02-13 22:45:00 +0800

    add file

--- src/org/foo/build.groovy
+++ src/org/foo/build.groovy
@@ -5,7 +5,7 @@ def Build(buildType,buildShell){
     println("当前选择的构建类型是${buildType}")
     buildHome=tool buildTools[buildType]
     if("${buildType}"=="npm"){
-        sh "export NODE_HOME=${buildHome} && export PATH=\$NODE_HOME/bin:\$PATH &&${NODE_HOME}/bin/npm ${buildShell}"
+        sh "export NODE_HOME=${buildHome} && export PATH=\$NODE_HOME/bin:\$PATH &&${buildHome}/bin/${buildType} ${buildShell}"
     }else{
         println(buildHome)
         sh "${buildHome}/bin/${buildType} ${buildShell}"
